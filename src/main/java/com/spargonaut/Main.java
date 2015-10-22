6329784a1e1b961e1b32f97d88395e5c30684a50
jamesspargo <jspargo@thoughtworks.com>
2015-10-21 22:04:28 -0500

    JS - implemented the usage of the Adder class in the main method;

--- src/main/java/com/spargonaut/Main.java
+++ src/main/java/com/spargonaut/Main.java
@@ -2,6 +2,13 @@ package com.spargonaut;
 
 class Main {
     public static void main(String[] args) {
-        System.out.println("hello world!");
+
+        int firstNumber = 22;
+        int secondNumber = 33;
+
+        Adder adder = new Adder();
+        int result = adder.addTwoInts(firstNumber, secondNumber);
+
+        System.out.println("\n\nI have added " + firstNumber + " and " + secondNumber + " for a result of: " + result);
     }
 }
