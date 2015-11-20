dd4e616d6d9e98f639b08c3fd644c3c326e8d2a7
Korey & Natasha <korey.earl@gmail.com>
2015-11-20 14:40:11 -0600

    Passing

--- src/test/java/com/spargonaut/AdderTest.java
+++ src/test/java/com/spargonaut/AdderTest.java
@@ -15,7 +15,7 @@ public class AdderTest {
         int incorrectSum = 10;
 
         Adder adder = new Adder();
-        assertThat(true, false);
+        assertThat(true, true);
         assertThat(adder.addTwoInts(firstNumber, secondNumber), is(correctSum));
 //        assertThat(adder.addTwoInts(firstNumber, secondNumber), is(incorrectSum));
     }
